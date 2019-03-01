c163c8031364a5655a7abd31c3847d868680ee3e
Yurii <davydovychyurii@gmail.com>
2019-03-01 11:54:52 +0000

    Inital

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-let year = prompt('Скільки вам років?');
-let msg = year - 2 + 3;
-alert(msg);
+const a = parseFloat(prompt('Введіть значення а'));
+const b = parseFloat(prompt('Введіть значення b'));
+const c = parseFloat(prompt('Введіть значення c'));
+
+function deck(a, b, c) {
+    return  b*b-4*a*c;
+}
+const resDesk=deck(a,b,c);
+function addDesk (a, b, c, resDesk){
+    if (resDesk > 0){
+        const x1=(-b+Math.sqrt(resDesk))/(2*a);
+        const x2=(-b-Math.sqrt(resDesk))/(2*a);
+        return 'Х1= '+ x1 + '; X2=' + x2;
+    }else if(resDesk === 0){
+        const x=(-b)/(2*a)
+        return "Рівняння має 1 корінь Х=" + x;
+    }else{
+        return 'Дескримінант <0 коренів рівняння немає';
+    }
+}
+alert(addDesk(resDesk));
+
+
